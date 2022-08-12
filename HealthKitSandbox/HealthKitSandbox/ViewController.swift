257f97a01e3224bfb776008eb0dab6b4daad11ae
sm-amoled <moktak072@naver.com>
2022-08-12 11:07:35 +0900

    HealthKit Sandbox

---
+++ HealthKitSandbox/HealthKitSandbox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthKitSandbox
+//
+//  Created by Park Sungmin on 2022/08/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
